e3de9d56c4cba542c406dec22352c91e084b6c2e
Jason Dusek <jason.dusek@gmail.com>
2011-11-18 01:40:53 +0000

    Work in progress; many changes. Main thrust of work is Route53 integration.

--- lib/zonify.rb
+++ lib/zonify.rb
@@ -28,15 +28,16 @@ class Capture
     @elb             = opts[:elb]
     suffixes         = (opts[:suffixes] or {})
     @suffixes        = {}
-    @suffixes[:host] = Zonify.dot((suffixes[:host] or '.ins'))
-    @suffixes[:elb]  = Zonify.dot((suffixes[:elb]  or '.elb'))
-    @suffixes[:sg]   = Zonify.dot((suffixes[:sg]   or  '.sg'))
+    @suffixes[:host] = Zonify._dot((suffixes[:host] or '.ins'))
+    @suffixes[:elb]  = Zonify._dot((suffixes[:elb]  or '.elb'))
+    @suffixes[:sg]   = Zonify._dot((suffixes[:sg]   or  '.sg'))
   end
   def instances
     @ec2.describe_instances.inject({}) do |acc, i|
       dns = i[:dns_name]
       unless dns.nil? or dns.empty?
-        acc[i[:aws_instance_id]] = {:dns => dns, :sg  => i[:aws_groups]}
+        acc[i[:aws_instance_id]] = { :sg  => i[:aws_groups],
+                                     :dns => Zonify.dot_(dns) }
       end
       acc
     end
@@ -51,14 +52,19 @@ class Capture
     hosts = instances
     elbs = load_balancers
     host_records = hosts.map do |id,info|
-      ['CNAME', "#{id}#{@suffixes[:host]}", info[:dns]]
-    end
+      [ { :type=>'CNAME', :ttl=>'86400',
+          :name=>"#{id}#{@suffixes[:host]}", :data=>info[:dns] },
+        { :type=>'TXT', :ttl=>'100',
+          :name=>'all', :data=>"zonify // #{info[:dns]}" } ]
+    end.flatten
     elb_records = elbs.map do |elb|
       running = elb[:instances].map{|i| hosts[i] }.compact
+      name = "#{elb[:prefix]}#{@suffixes[:elb]}",
       running.map do |host|
-        ['TXT', "#{elb[:prefix]}#{@suffixes[:elb]}", "zonify/#{host[:dns]}"]
+        { :type=>'TXT', :ttl=>'100',
+          :name=>name, :data=>"zonify // #{host[:dns]}" }
       end
-    end.flatten(1)
+    end.flatten
     sg_records = hosts.inject({}) do |acc, kv|
       _, info = kv
       info[:sg].each do |sg|
@@ -68,11 +74,47 @@ class Capture
       acc
     end.map do |sg, hostnames|
       sg_ldh = Zonify.sg_name_to_ldh(sg)
+      name = "#{sg_ldh}#{@suffixes[:sg]}"
       hostnames.map do |hostname|
-        ['TXT', "#{sg_ldh}#{@suffixes[:sg]}", "zonify/#{hostname}"]
+        { :type=>'TXT', :ttl=>'100',
+          :name=>name, :data=>"zonify // #{hostname}" }
       end
-    end.flatten(1)
-    [host_records, elb_records, sg_records].flatten(1)
+    end.flatten
+    [host_records, elb_records, sg_records].flatten
+  end
+end
+
+class Sync
+  attr_accessor :r53, :root
+  def initialize(root, opts={})
+    @r53             = opts[:r53]
+    @root            = Zonify::_dot(Zonify::dot_(root))
+  end
+  def calculate_change_set(captured)
+  end
+  def retrieve_zone_and_records
+    relevant_zone = @r53.list_hosted_zones.select do |zone|
+      @root.end_with?(zone[:name])
+    end.sort_by{|zone| zone[:name].length }.last
+    abort "No relevant zone." unless relevant_zone
+    zone_id = relevant_zone[:aws_id]
+    relevant_records = @r53.list_resource_record_sets(zone_id).select do |rr|
+      rr[:name].end_with?(@root)
+    end
+    [relevant_zone, relevant_records]
+  end
+  class << self
+    def convert_to_route53_form(captured)
+      captured.inject({}) do |acc, record|
+        name, type, ttl, data = [ record[:name], record[:type],
+                                  record[:ttl],  record[:data]  ]
+        acc[name]                          ||= {}
+        acc[name][type]                    ||= { :ttl => ttl }
+        acc[name][type][:resource_records] ||= []
+        acc[name][type][:resource_records] << data
+        acc
+      end
+    end
   end
 end
 
@@ -89,8 +131,12 @@ def sg_name_to_ldh(s)
                                    sub(/(^-|-$)/, '0')
 end
 
-def dot(s)
-  s.sub(/^[^.]/, ".#{$1}")
+def _dot(s)
+  /^[.]/.match(s) ? s : ".#{s}"
+end
+
+def dot_(s)
+  /[.]$/.match(s) ? s : "#{s}."
 end
 
 end
